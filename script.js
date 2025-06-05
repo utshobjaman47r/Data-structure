e08b6d05cce3a6579dedd34f422c97b2eac29b55
UTSHOB <utshobjaman2012@gmail.com>
2025-06-05 09:53:22 +0600

    destractaring

---
+++ script.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const resturent = {
+    name : 'classico Italino ',
+    locaiton :'via angelo Tavanti 23, firenze , Italy',
+    categories : ['Italian ', 'Pizzeria', 'Vegetarian ', 'organic '],
+    startMenu :['Focaccia ', 'Bruschetta ', 'Garlic', 'Bread ', 'cprese salad '],
+    mainManu : ['Pizza', 'pasta ']
+
+}
+
+
+
+const arr = [2,3,4];
+const a =arr[0];
+const b =arr[1];
+const c =arr[2];
+
+
+const [x,y,z] = arr;
+console.log(c)
+
+console.log(x,y,z);
+
+console.log(arr)
